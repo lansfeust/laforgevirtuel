d27b06f61965510ebc525c921a0614a2239a3585
lansfeust <lansfeust@hotmail.fr>
2022-04-04 09:55:55 +0200

    ajout d'aide , markdown et page par defaut

---
+++ autorisationENTREE/Formatage Markdown.md
@@ -0,0 +1,39 @@
+# Titre nv 1 `#`
+## Titre nv 2 `##`
+### Titre nv 3 `###`
+*`quelques mots*
+**quelques mots**  
+pour mettre a la ligne simplement   
+2 espaces a la fin de la ligne  
+
+## texte en `code`
+    Première ligne de code
+    Deuxième ligne
+***quelques `code` mots grace a `alt` + `7`***
+
+## élément HTML 
+> Ce texte apparaîtra dans un élément HTML 
+<blockquote>comme un blockquote</blockquote>
+
+>Pour créer des tableaux (balises HTML `<tr>` et `<th>`)
+>>Pour créer des liens (balise HTML <a>) :
+>`[texte du lien]`(url_du_lien "texte pour le titre, facultatif")
+
+Pour ***afficher une image*** (balise HTML <img>) :
+`![Texte alternatif]` ( `url_de_l'image` "texte pour le titre, facultatif")
+
+
+
+## Les listes
+* liste = `*` + `espace`
+   * sous - liste = `tab` + `*` + `espace`
+
+1. liste ordonné
+2. grace a `nombre` + `.`
+
+- [ ] Case non cochée `-` + `[]` crochet
+- [x] Case cochée  `-` + `[x]` crochet avec un x dedans
+
+
+
+
---
+++ autorisationENTREE/page_par_defaut.py
@@ -0,0 +1,46 @@
+"""
+
+Nom du projet : Page_Par_Defaut
+
+Date de la dernière révision : 24/03/2022
+
+Auteur(s) : Lansfeust
+
+Révision N° : Version 1
+
+Client : Lansfeust
+
+Fichiers du projet :
+					-	main.py
+					
+				
+"""
+
+# Indispensable hors contexte de PyCharm->en bas à droite
+# -*- coding:Utf-8 -*-
+
+################################################
+#				Programme principal
+################################################
+
+#-----------------------------------------------
+#		    Zone des 'imports' de modules
+#-----------------------------------------------
+
+from tkinter import *
+
+#----------------------------------------------------
+#		Zone de déclaration des variables globales
+#----------------------------------------------------
+
+
+#-------------------------------------------------------
+#		Zone de déclaration des modules ou des fonctions
+#-------------------------------------------------------
+
+
+#-------------------------------------------------------
+#						PROGRAMME
+#-------------------------------------------------------
+
+
